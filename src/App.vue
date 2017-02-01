8575ad5cc5879e797a8a2f3589dc715259699e19
Sylvain Santucci <sylvain.santucci@gmail.com>
2017-02-01 23:39:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <NavbarTop></NavbarTop>
+  </div>
+</template>
+
+<script>
+import NavbarTop from './components/common/NavbarTop'
+
+export default {
+  name: 'app',
+  components: {
+    NavbarTop
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/common/NavbarTop.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>mon test</header>
+</template>
+
+<script>
+export default {
+  name: 'NavbarTop',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
