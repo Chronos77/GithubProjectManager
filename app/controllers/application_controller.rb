7b4e2dd1af56cd58791258fb76155f84cf9b4a28
Sylvain Santucci <sylvain.santucci@gmail.com>
2017-02-17 17:40:09 +0100

    ajout de la redirection vers la home et le confirmation auto si il passe par github(peut etre à revoir)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :authenticate_user!
   def get_resources(url)
     open("#{GITHUB_BASE_URL}#{url}",
       "Accept" => "application/vnd.github.v3+json",
